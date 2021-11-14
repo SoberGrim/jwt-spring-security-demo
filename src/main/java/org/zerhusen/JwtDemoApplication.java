61deb96c0ff07caafe1aab3edbf5d3b0a39a3862
Александр Моисеев <sobergrim@gmail.com>
2021-11-14 21:17:34 +0300

    v 1.1

--- src/main/java/org/zerhusen/JwtDemoApplication.java
+++ src/main/java/org/zerhusen/JwtDemoApplication.java
@@ -3,10 +3,18 @@ package org.zerhusen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class JwtDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(JwtDemoApplication.class, args);
+       openHomePage();
     }
+
+   private static void openHomePage() throws IOException {
+      Runtime rt = Runtime.getRuntime();
+      rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8090/");
+   }
 }
